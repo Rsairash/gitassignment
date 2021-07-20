16dfbca83f637fe8d874746ed27ea2c64709c937
Sairash <sairash0323@gmail.com>
2021-07-20 22:30:07 +0545

    count implemented

--- assignment.c
+++ assignment.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int a,b,sum;
+    int a,b,sum,i;
     a=5;
     b=9;
     sum=a+b;
-    printf("The sum is %d",sum);
+    printf("The sum is %d\n",sum);
+
+    for(i=1;i<=10;i++)
+    {
+        printf("%d\t",i);
+    }
 }
